bf49015e267490efeefc55b1f29d699a302af89f
tonyfaros <tonyfaros@gmail.com>
2018-03-17 23:00:40 -0600

    finished mp3Player with lyrics and listview, volume and advance seekbar

--- app/src/main/java/com/example/anthony_pc/mp3player/MainActivity.java
+++ app/src/main/java/com/example/anthony_pc/mp3player/MainActivity.java
@@ -10,14 +10,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Timer;
@@ -31,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
     ListView lvSongs;
     final String[] songs = {"Eminem - Cleanin' out my closet","Eminem - The real slim shady",
     "Judah the lion - Take it all back","Metallica - Mama said"};
+
+    final String[] songsLyrics = {"cleanin_out_my_closet","the_real_slim_shady",
+            "take_it_all_back","mama_said_lyrics"};
     final int[] resID = {R.raw.eminemcleaninoutmycloset, R.raw.eminemtherealslimshady, R.raw.judahtheliontakeitallback, R.raw.mamasaidmetallica};
     MediaPlayer mediaPlayer;
     SeekBar advanceSB, volumeSB;
@@ -38,7 +48,15 @@ public class MainActivity extends AppCompatActivity {
 
     Handler handler;
     Runnable runnable;
-    int currentSong = -1;
+    int currentSong = 0;
+
+    StringBuilder lyrics = new StringBuilder();
+    ScrollView scrollView;
+
+    TextView tvLyrics;
+
+    int lengthSB = 0;
+    int lengthSCB = 0;
 
     AudioManager audioManager;
     @Override
@@ -46,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Typeface font = Typeface.createFromAsset( getAssets(), "fontawesome-webfont.ttf" );
+        tvLyrics = findViewById(R.id.TVLyrics);
+        scrollView = findViewById(R.id.scrollViewLyrics);
         mediaPlayer = MediaPlayer.create(getApplicationContext(), resID[0]);
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         volumeSB = findViewById(R.id.volumeSB);
@@ -72,7 +92,8 @@ public class MainActivity extends AppCompatActivity {
         advanceSB = findViewById(R.id.advanceSB);
 
 
-
+        lengthSB = mediaPlayer.getDuration();
+        lengthSCB = scrollView.getChildAt(0).getHeight();
 
 
         advanceSB.setMax(mediaPlayer.getDuration());
@@ -83,15 +104,12 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,  android.R.layout.simple_list_item_1, songs);
         lvSongs.setAdapter(adapter);
 
+        scrollView.scrollTo(0,300);
         lvSongs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                mediaPlayer.stop();
-                mediaPlayer = MediaPlayer.create(getApplicationContext(), resID[i]);
                 currentSong = i;
-                advanceSB.setMax(mediaPlayer.getDuration());
-                mediaPlayer.start();
-                songCycle();
+                songNextChange();
             }
         });
 
@@ -117,10 +135,9 @@ public class MainActivity extends AppCompatActivity {
         advanceSB.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean input) {
-                if(input)
+                if(input) {
                     mediaPlayer.seekTo(progress);
-
-
+                }
             }
 
             @Override
@@ -140,6 +157,9 @@ public class MainActivity extends AppCompatActivity {
         if((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)){
             volumeSB.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
         }
+        if((keyCode == KeyEvent.KEYCODE_BACK)){
+            finish();
+        }
         return super.onKeyDown(keyCode, event);
 
     }
@@ -155,7 +175,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void songCycle(){
         advanceSB.setProgress(mediaPlayer.getCurrentPosition());
-
+        int cont = (mediaPlayer.getCurrentPosition() * 100) / lengthSB;
+        int cont2 = (cont * lengthSCB) / 100;
+        scrollView.scrollTo(0,cont2-400);
         if(mediaPlayer.isPlaying()){
             runnable = new Runnable() {
                 @Override
@@ -164,6 +186,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
             handler.postDelayed(runnable,1000);
+        }
+        if(mediaPlayer.getCurrentPosition() == lengthSB){
+            clickNextSong(null);
+
         }
     }
 
@@ -190,42 +216,97 @@ public class MainActivity extends AppCompatActivity {
     public void clickPlay(View view){
 
         if(mediaPlayer.isPlaying()){
+            Log.e("playing","playing");
             mediaPlayer.pause();
-            imageBtnPlay_Stop.setText(String.valueOf((char) 0xf28d));
-        }else{
-            mediaPlayer.start();
             imageBtnPlay_Stop.setText(String.valueOf((char) 0xf144));
+        }else{
+            Log.e("playing","stop");
+            mediaPlayer.start();
+            imageBtnPlay_Stop.setText(String.valueOf((char) 0xf28d));
             songCycle();
         }
     }
 
     public void clickNextSong(View view){
-        mediaPlayer.stop();
+        //mediaPlayer.stop();
         if(currentSong == resID.length-1) {
             currentSong = 0;
-            mediaPlayer = MediaPlayer.create(getApplicationContext(), resID[currentSong]);
+            songNextChange();
+            //mediaPlayer = MediaPlayer.create(getApplicationContext(), resID[currentSong]);
         }else{
-            mediaPlayer = MediaPlayer.create(getApplicationContext(), resID[currentSong+1]);
+            //mediaPlayer = MediaPlayer.create(getApplicationContext(), resID[currentSong+1]);
             currentSong +=1;
+            songNextChange();
         }
-        mediaPlayer.start();
-        songCycle();
+        //mediaPlayer.start();
+       // songCycle();
 
 
     }
     public void clickPreviousSong(View view){
         Log.e("id",String.valueOf(currentSong));
-        mediaPlayer.stop();
+        //mediaPlayer.stop();
         if(currentSong == 0){
-            mediaPlayer = MediaPlayer.create(getApplicationContext(), resID[resID.length-1]);
+            //mediaPlayer = MediaPlayer.create(getApplicationContext(), resID[resID.length-1]);
+
             currentSong = resID.length-1;
+            songNextChange();
         }else{
-            mediaPlayer = MediaPlayer.create(getApplicationContext(), resID[currentSong-1]);
+            //mediaPlayer = MediaPlayer.create(getApplicationContext(), resID[currentSong-1]);
+
             currentSong-=1;
+            songNextChange();
         }
+        //mediaPlayer.start();
+        //songCycle();
+    }
+
+    public void songNextChange(){
+        mediaPlayer.stop();
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), resID[currentSong]);
+        //currentSong = i;
+        advanceSB.setMax(mediaPlayer.getDuration());
         mediaPlayer.start();
         songCycle();
+        imageBtnPlay_Stop.setText(String.valueOf((char) 0xf28d));
+
+        BufferedReader reader = null;
+        lyrics = new StringBuilder();
+
+
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(getAssets().open(songsLyrics[currentSong]+".txt")));
+
+            // do reading, usually loop until end of file reading
+            String mLine;
+            while ((mLine = reader.readLine()) != null) {
+                lyrics.append(mLine);
+                lyrics.append('\n');
+            }
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(),"Error reading file!",Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    //log the exception
+                }
+            }
+        }
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.animationfile);
+        animation.setDuration(mediaPlayer.getDuration() * 1000);
+        tvLyrics.setText(lyrics);
+        lengthSB = mediaPlayer.getDuration();
+        lengthSCB = scrollView.getChildAt(0).getHeight();
+        tvLyrics.startAnimation(animation);
+        animation.start();
+
     }
+
 
 
 }
